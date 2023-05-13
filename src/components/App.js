c9c73edf491a456ddde9a3ff78077760cd8c72b7
MarcusDantas <marvinsd@gmail.com>
2023-05-13 14:34:38 -0300

    feat: add keybord logic

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,25 @@ import Jogo from "./Jogo/Jogo";
 import Letras from "./Letras/Letras";
 
 export default function App() {
+    const [enabledKeybord, setEnabledKeybord] = React.useState(false);
+    const [disabledKeys, setDisabledKeys] = React.useState([]);
+
+    function startGame() {
+        setEnabledKeybord(true);
+        setDisabledKeys([]);
+    }
+
+    function handleKeyClick(index) {
+        setDisabledKeys([...disabledKeys, index]);
+    }
+
     return (
         <>
             <GlobalStyle />
-            <Jogo></Jogo>
-            <Letras></Letras>
+            <Jogo onClick={startGame}></Jogo>
+            <Letras enabledKeyboard={enabledKeybord}
+                disabledKeys={disabledKeys}
+                onKeyClick={handleKeyClick}></Letras>
         </>
     );
 }
